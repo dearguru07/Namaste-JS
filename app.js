b50296813ac7a8857a044c8576641139d538b3de
dearguru07 <pangalaguruyadav101@gmail.com>
2025-12-05 22:10:00 +0530

    f

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 // console.log('Hello world...')
 
-
 // let a =10;
 // console.log(a);
 // var b=100;
 
-
-
 // const z=100;
 // z=25;
 
+// var a=10;
+// let b=20;
+// const c=100;
+// {
+//     let x=25;
+//     const z=2547;
+// }
 
-var a=10;
-let b=20;
-const c=100;
-{
-    let x=25;
-    const z=2547;
+var x = 10;
+function z() {
+    let c=25;
+  function m() {
+    console.log(c);
+  }
+  m();
 }
+z();
