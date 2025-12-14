cbdbc7a2a7be0e2428677df3c4a0e9b3b5bc38fa
dearguru07 <pangalaguruyadav101@gmail.com>
2025-12-14 15:56:14 +0530

    chs

--- app.js
+++ app.js
@@ -15,12 +15,31 @@
 //     const z=2547;
 // }
 
-var x = 10;
-function z() {
-    let c=25;
-  function m() {
-    console.log(c);
-  }
-  m();
+// var x = 10;
+// function z() {
+//     let c=25;
+//   function m() {
+//     console.log(c);
+//   }
+//   m();
+// }
+// z();
+
+
+console.log('Start');
+setTimeout(()=>{
+  console.log("Callback")
+},3000);
+console.log("End");
+
+let startDate=new Date().getTime();
+let endDate=startDate;
+while(endDate<startDate+10000){
+  endDate=new Date().getTime()
 }
-z();
+console.log("While Expires");
+
+console.log("Namastee")
+setTimeout(()=>{
+  console.log('Guru')
+},1000)
