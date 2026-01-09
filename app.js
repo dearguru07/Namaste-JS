7ed8423e40e03df9cf8bc0e9867ad75b9f5226e8
dearguru07 <pangalaguruyadav101@gmail.com>
2026-01-09 19:56:26 +0530

    f

--- app.js
+++ app.js
@@ -215,3 +215,221 @@ var addTwoNumbers = function (l1, l2) {
     }
     return headSol.next;
 };
+
+// console.log('Hello world...')
+
+// let a =10;
+// console.log(a);
+// var b=100;
+
+// const z=100;
+// z=25;
+
+// var a=10;
+// let b=20;
+// const c=100;
+// {
+//     let x=25;
+//     const z=2547;
+// }
+
+// var x = 10;
+// function z() {
+//     let c=25;
+//   function m() {
+//     console.log(c);
+//   }
+//   m();
+// }
+// z();
+
+// console.log('Start');
+// setTimeout(()=>{
+//   console.log("Callback")
+// },3000);
+// console.log("End");
+
+// let startDate=new Date().getTime();
+// let endDate=startDate;
+// while(endDate<startDate+10000){
+//   endDate=new Date().getTime()
+// }
+// console.log("While Expires");
+
+// console.log("Namastee")
+// setTimeout(()=>{
+//   console.log('Guru')
+// },1000)
+
+let radius = [7, 5, 4, 1];
+
+let calculateArea = function (radius) {
+  let output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(Math.PI * radius[i] * radius[i]);
+  }
+  return output;
+};
+console.log(calculateArea(radius));
+
+let CalucateCurmferice = function (radius) {
+  let output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(2 * Math.PI * radius[i]);
+  }
+  return output;
+};
+console.log(CalucateCurmferice(radius));
+
+
+let CalculateDaimeter = function (radius) {
+  let output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(2 *radius[i]);
+  }
+  return output;
+};
+console.log(CalculateDaimeter(radius));
+
+
+
+function Node(val) {
+    this.val = val;
+    this.next = null;
+}
+var MyLinkedList = function () {
+    this.size = 0;
+    this.head = null;
+};
+
+/** 
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function (index) {
+    if (index < 0 || index >= this.size) return -1;
+    else {
+        let curr = this.head;
+        for (let i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        return curr.val;
+    }
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function (val) {
+    let headNode = new Node(val);
+    headNode.next = this.head;
+    this.head = headNode;
+    this.size++;
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function (val) {
+    let tailNode = new Node(val);
+    if (this.head === null) {
+        this.head = tailNode;
+    }
+    else {
+        let curr = this.head;
+        while (curr.next) {
+            curr = curr.next;
+        }
+        curr.next = tailNode;
+    }
+    this.size++;
+};
+
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function (index, val) {
+    if (index < 0 || index > this.size) return;
+    let indexNode = new Node(val);
+    if (index === 0) {
+        this.addAtHead(val);
+        return;
+    }
+    if (index === this.size) {
+        this.addAtTail(val);
+        return;
+    }
+    else {
+        let curr = this.head;
+        for (let i = 0; i < index - 1; i++) {
+            curr = curr.next;
+        }
+        indexNode.next = curr.next;
+        curr.next = indexNode;
+    }
+    this.size++;
+};
+
+/** 
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+    if (index < 0 || index >= this.size) return;
+    if (index === 0) {
+        this.head = this.head.next;
+    }
+    else {
+        let curr = this.head;
+        for (let i = 0; i < index - 1; i++) {
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+    }
+    this.size--;
+};
+
+/** 
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+var addTwoNumbers = function (l1, l2) {
+    let sol = new ListNode();
+    let headSol = sol;
+    let carry = 0
+    while (l1 || l2 || carry) {
+        let sum = (!l1 ? 0 : l1.val) + (!l2 ? 0 : l2.val) + carry;
+        carry = Math.floor(sum / 10);
+        let digit = sum % 10;
+        let newNode = new ListNode(digit);
+        sol.next = newNode;
+        sol = sol.next;
+        l1 = l1 && l1.next;
+        l2 = l2 && l2.next;
+    }
+    return headSol.next;
+};
