fe4d3cbf935af2640c753d761b769d83a9bbea5c
dearguru07 <pangalaguruyadav101@gmail.com>
2026-02-07 11:05:24 +0530

    pr

--- index.js
+++ index.js
@@ -10,10 +10,111 @@
 //   // console.log(i,j)
 // }
 
+// let n=20;
+// console.log(n);
+// function getName(){
+//     console.log('Hello');
+// }
+// getName()
 
-let n=20;
-console.log(n);
-function getName(){
-    console.log('Hello');
+// let n=5;
+// let toggle = 1;
+
+// for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j <= i; j++) {
+//         row = row + toggle;
+//         if (toggle == 0) {
+//             toggle = 1;
+//         }
+//         else {
+//             toggle = 0;
+//         }
+//     }
+//     console.log(row);
+// }
+
+const cart = [
+  {
+    itemName: "Shoes",
+    itemPrice: 2000,
+  },
+  {
+    itemName: "Paint",
+    itemPrice: 2500,
+  },
+  {
+    itemName: "Kurta",
+    itemPrice: 1500,
+  },
+];
+
+let walletBalance = 10000;
+
+function createOrder(cart) {
+  return new Promise(function (resolve, reject) {
+    if (!validateCart(cart)) {
+      reject(new Error("Cart is not valid"));
+    }
+    let orderId = 10;
+    if (orderId) {
+      resolve(orderId);
+    }
+  });
 }
-getName()
+
+function proceedToPayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    if (orderId) {
+      resolve({ paymentStatus: 1, message: "Payment successfully completed" });
+    } else {
+      reject(new Error("Payment Failed"));
+    }
+  });
+}
+
+function showOrderSummery(orderStatus) {
+  return new Promise(function (resolve, reject) {
+    if (orderStatus.paymentStatus === 1) {
+      resolve({ status: "success", orders: cart });
+    } else {
+      reject(new Error("Something went wrong"));
+    }
+  });
+}
+
+function updateWallet(orderHistory) {
+  return new Promise(function (resolve, reject) {
+    if (orderHistory.status === "success") {
+      let orderAmount = 6000;
+      walletBalance = walletBalance - orderAmount;
+      resolve({ balance: walletBalance, message: "Wallet updated" });
+    } else {
+      reject(new Error("Wallet balance not updated"));
+    }
+  });
+}
+
+function validateCart(cart) {
+  return true;
+}
+
+createOrder(cart)
+  .then(function (orderId) {
+    return orderId;
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (orderStatus) {
+    return showOrderSummery(orderStatus);
+  })
+  .then(function (orderHistory) {
+    return updateWallet(orderHistory);
+  })
+  .then(function (res) {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
