4c8610a438d191b34c998b5de78c494098952d10
dearguru07 <pangalaguruyadav101@gmail.com>
2026-03-02 20:00:51 +0530

    'this'

---
+++ swiggy.js
@@ -0,0 +1,58 @@
+// "use strict"
+
+// const fetchData = async () => {
+// //   try {
+// //     const response = await fetch(
+// //       "https://www.swiggy.com/city/bangalore/social-church-street-rest17118?restaurant_id=17118&source=collection&query=Paratha"
+// //     );
+
+// //     const json = await response.json();
+// //     console.log(json);
+
+// //     const respones = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+// //     console.log(respones);
+// //   } catch (error) {
+// //     console.error('Error:', error);
+// //   }
+// const data=await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9856503&lng=77.60569269999999&restaurantId=17118&catalog_qa=undefined&query=Paratha&submitAction=ENTER");
+// const json=await data.json();
+// console.log(json)
+// };
+// fetchData();
+
+// console.log(this);
+
+// function x(){
+//     console.log(this);
+// }
+// x();
+// window.x();
+
+// let obj={
+//     a:10,
+//     b:function (){
+//         console.log(this.a)
+//     }
+// }
+// obj.b();
+
+// let student={
+//     name:"Guru",
+//     printName:function(){
+//         console.log(this.name);
+//     }
+// }
+// student.printName();
+
+// let student2={
+//     name:"Deeepika",
+// }
+// student.printName.call(student2)
+
+let obj = {
+  a: 10,
+  b: () => {
+    console.log(this);
+  },
+};
+obj.b();
